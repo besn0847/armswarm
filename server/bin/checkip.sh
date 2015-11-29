10b9bae2abdc8b9f9a192b1bebcb8b87cf815375
besn0847 <franck@besnard.mobi>
2015-11-29 09:45:52 +0100

    Fixing socat + ip change issues

---
+++ server/bin/checkip.sh
@@ -0,0 +1,15 @@
+#!/bin/ash
+
+while(true)
+do
+   SWARMIP=`ps -ef | grep join | grep -v grep |  awk '{print$6}' - | awk -F"=" '{print$2}' - | awk -F":" '{print$1}'`
+   CURRENTIP=`ip addr show tun0 | grep inet | awk '{print $2}'`
+
+   if [ $SWARMIP != $CURRENTIP ]
+   then
+      echo `date`" : @IP misalignement, restarting Swarm agent"
+      kill `ps -ef | grep join | grep -v grep |  awk '{print$1}' -` 
+   fi
+
+   sleep 60
+done
