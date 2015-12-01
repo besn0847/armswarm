770863f602f8598a82aae9505a220328e249fde3
Franck Besnard <franck@besnard.mobi>
2015-12-01 14:55:52 +0100

    fixed checkip.sh script

--- nodes/x86/bin/checkip.sh
+++ nodes/x86/bin/checkip.sh
@@ -7,7 +7,7 @@ do
    SWARMIP=`ps -ef | grep join | grep -v grep |  awk '{print$6}' - | awk -F"=" '{print$2}' - | awk -F":" '{print$1}'`
    CURRENTIP=`ip addr show tun0 | grep inet | awk '{print $2}'`
 
-   if [ $SWARMIP != $CURRENTIP ]
+   if [ x"$SWARMIP" != x"$CURRENTIP" ]
    then
       echo `date`" : @IP misalignement, restarting Swarm agent"
       kill `ps -ef | grep join | grep -v grep |  awk '{print$1}' -` 
