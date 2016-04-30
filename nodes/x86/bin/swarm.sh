69afd326ab7082079e3b20119f55c7f02597b8b0
Franck Besnard <franck@besnard.mobi>
2016-04-30 12:37:54 +0200

    Fixed ip v6 issue in swarm.sh

--- nodes/x86/bin/swarm.sh
+++ nodes/x86/bin/swarm.sh
@@ -1,3 +1,3 @@
 #!/bin/ash
 
-/bin/swarm -l "warn" join --addr=`ip addr show tun0 | grep inet | awk '{print $2}'`:23750 $*
+/bin/swarm -l "warn" join --addr=`ip addr show tun0 | grep inet | awk '{print $2}' | egrep -v '[a-z]'`:23750 $*
